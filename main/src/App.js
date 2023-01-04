2de5cc7795a636f3d12dc21bf21188f536172835
bilguun-enkh <bilguunenkh21@gmail.com>
2023-01-04 23:05:55 +0800

    01/04/2023 finished search div and began browse dropdown menu

--- main/src/App.js
+++ main/src/App.js
@@ -4,32 +4,52 @@ import { Truck, GeoAlt, Heart, Person, CartFill } from 'react-bootstrap-icons';
 function App() {
   return (
     <div>
-      <div className="container">
+      <div className="my-container">
 
-        <div className="nav-bar">
+        <div className="my-nav-bar">
           <div>Need help? Call us: (+98) 0234 456 789</div>
           <div>
             <div className="nav-bar-2nd-div">
-              <span><GeoAlt />Our store</span>
-              <span><Truck />Track your order</span>
+              <span className="our-store"><GeoAlt />Our store</span>
+              <span className="track-order"><Truck />Track your order</span>
             </div>
           </div>
         </div>
       </div>
 
       <div className="searchContainer">
-        <div class="searchDiv">
-          <div class="logoDiv">
-            <img src="electronLogo.png" class="searchDivLogo"></img>
+        <div className="searchDiv">
+          <div className="logoDiv">
+            <img src="electronLogo.png" alt="electronlogo" className="searchDivLogo"></img>
             <div className='searchBox'>
-              <input></input>
+              <input placeholder='Search any things' className="searchInput"></input>
+              <button className="search-btn">Search</button>
             </div>
           </div>
           <div className='register'>
-            <span><Person />Sign In</span>
+            <span className='sign-in'><Person />Sign In</span>
             <span><Heart /></span>
             <span><CartFill /></span>
           </div>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='browse-div'>
+          <div class="btn-group">
+            <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+              Action
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+          </div>
+          <div></div>
+          <div></div>
         </div>
       </div>
     </div>
